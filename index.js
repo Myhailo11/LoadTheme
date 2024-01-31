4c18d14e12eb62572c1a0dea24fdcac2a7bc29ad
us5nar <us5nar@gmail.com>
2024-01-31 21:19:32 +0200

    fixed NULL value, fixed Scheme toggling, refactored

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    const themToggleBtn = document.querySelector(".btn")
-    const currentThem = localStorage.getItem("them")
+    const colorSchemeToggleButton = document.querySelector(".btn");
+    //=====Get reed of NULL using ternary operator====
+    const currentColorScheme = localStorage.getItem("colorScheme") ? localStorage.getItem("colorScheme") : "light";
+    console.log(`Vale from Local Storage:`, localStorage.getItem("colorScheme"));
+    console.log(`Purefie Value even if null:`, currentColorScheme);
+    //===============================================
+    applyColorScheme(currentColorScheme);
+    
+    colorSchemeToggleButton.addEventListener("click", () => {
+        const newColorScheme = document.body.classList.contains("dark") ? "light" : "dark";
+        applyColorScheme(newColorScheme);
+        //Saving the Scheme to the local Storage
+        localStorage.setItem("colorScheme", newColorScheme);
+        console.log(`Vale from Local Storage:`, localStorage.getItem("colorScheme"));
+    });
 
-
-    if (currentThem) {
-        applyThem(currentThem)
-    }
-
-    themToggleBtn.addEventListener("click", () => {
-        const newThem = document.body.classList.contains("dark") ? "light" : "dark"
-        applyThem(newThem)
-        localStorage.setItem("them", newThem)
-    })
-
-    function applyThem(them) {
+    function applyColorScheme(colorScheme) {
         document.body.classList.remove("dark", "light")
-        document.body.classList.add(them)
+        document.body.classList.add(colorScheme)
     }
 })
 
